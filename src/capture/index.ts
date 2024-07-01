5ff5c21cdbf8cbe2cfd930929a172f2cb2aedc6f
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-07-01 17:03:25 +0900

    fix: 正常にログインできないことがある問題を修正

--- src/capture/index.ts
+++ src/capture/index.ts
@@ -78,8 +78,8 @@ export async function get(type: keyof typeof ApiType): Promise<any> {
         })
 
         if (!page.url().startsWith('https://www.fanbox.cc')) {
-            await page.type('input[autocomplete="username"]', config.pixiv.email);
-            await page.type('input[autocomplete="current-password"]', config.pixiv.password);
+            await page.type('input[autocomplete~="username"]', config.pixiv.email);
+            await page.type('input[autocomplete~="current-password"]', config.pixiv.password);
             await page.click('div+button[type="submit"]')
             await page.waitForNavigation({
                 waitUntil: ['load', 'networkidle2'],
